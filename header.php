655269ca8b57d351e84f7f37929a3fa6a300f4fd
jafary <jafaryesmaeil340@gmail.com>
2019-12-29 22:15:40 +0330

    header edite

--- header.php
+++ header.php
@@ -5,9 +5,9 @@
 <title> فروشگاه بچه های ای تی</title>
 	<!-- start header -->
 	<div class="header">
-		<span class="title">
+		<!--<span class="title">-->
 			<img src="images/همه چی کالا.gif">
-			</span>
+			<!--</span>-->
 			
 		<!--<span class="service"><h3>ارایه کننده محصولات فناوری اطلاعات</h3></span>
 		<span class="logo"><img src="images/Untitled-1.png"</span>-->
