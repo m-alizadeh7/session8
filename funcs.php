9c40ae9c5023af509ea9354ff24391393afb3e0f
jafary <jafaryesmaeil340@gmail.com>
2019-12-30 22:42:27 +0330

    run ignore -1

--- funcs.php
+++ funcs.php
@@ -1,12 +1,7 @@
 <?php 
-	/*function connect()
-	{
-		mysql_connect("localhost","root","") or die("can not connect to server...!");
-	mysql_query("SET NAMES 'utf8'");
-	mysql_select_db("jafary") or die("can not open database...!");
-		
-	*/
-		$connect = mysqli_connect("localhost","cp34008_jafary","jafary73845","cp34008_jafary");
+include "sqli.php";
+	
+		$connect = mysqli_connect($adddb,$dbuser,$dbpass,$dbname);
 		MySQLi_query($connect,"SET CHARACTER SET utf8");
 /*mysqli_query($connect ,"SET NAMES 'utf8'");*/
 
